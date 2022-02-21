e0a2fc7008cc4b7ba73aa38af8e35f474056a842
Andrew Barr <andrew@foxtail.consulting>
2022-02-21 12:51:44 +1000

    fixed offset when using svg items and svg parent

--- assets/js/svg.js
+++ assets/js/svg.js
@@ -2,36 +2,40 @@ export default {
     selectedElement: false,
     offset: false,
     
-    mounted() {    
+    mounted() { 
         this.el.addEventListener("mousedown", this.startDrag, false)
         this.el.addEventListener("mousemove", this.drag, false)
         this.el.addEventListener("mouseup", this.endDrag, false)
         this.el.addEventListener("mouseleave", this.endDrag, false)
     },
     startDrag(event) {
-        if (event.target.classList.contains('draggable')) {
-            this.selectedElement = event.target
-            this.offset = getMousePosition(event);
+        event.preventDefault()
+        if (event.target.parentNode.classList.contains('draggable')) {
+            this.selectedElement = event.target.parentNode
+            this.offset = getMousePosition(event, this.selectedElement);
+            console.log(this.offset)
             this.offset.x -= parseFloat(this.selectedElement.getAttributeNS(null, "x"));
             this.offset.y -= parseFloat(this.selectedElement.getAttributeNS(null, "y"));
           }
     },
     drag(event) {
+        event.preventDefault()
         if (this.selectedElement) {
-            event.preventDefault()
-            var coord = getMousePosition(event);
+            var coord = getMousePosition(event, this.selectedElement);
             this.selectedElement.setAttributeNS(null, "x", coord.x - this.offset.x);
             this.selectedElement.setAttributeNS(null, "y", coord.y - this.offset.y);
         }
     },
     endDrag(event) { 
-        this.selectedElement = false
+        event.preventDefault()
+        this.selectedElement =
+         false
     }
     
 }
 
-function getMousePosition(event) {
-    var CTM = event.target.getScreenCTM()
+function getMousePosition(event, selectedElement) {
+    var CTM = selectedElement.parentNode.getScreenCTM()
     return {
       x: (event.clientX - CTM.e) / CTM.a,
       y: (event.clientY - CTM.f) / CTM.d
